cdf055b984a3a61804e8488fa106fac31cc033d2
miyata <rm.eilsystem@gmail.com>
2021-11-15 15:05:07 +0900

    タグの追加

--- index.php
+++ index.php
@@ -7,10 +7,20 @@
     <link rel="canonical" href="https://amp-sample.herokuapp.com/">
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+    <style amp-custom>
+      p {
+        color: red;
+      }
+      amp-img {
+        border: 3px solid blue;
+      }
+    </style>
   </head>
   <body>
     <h1 id="hello">Hello AMPHTML World!</h1>
     <p>php</p>
+    <amp-img src="/assets/img/dummy.png" alt="dummy" width="150" height="150"></amp-img>
+    <amp-twitter width="400" height="600" layout="responsive" data-tweet-id="1459902068664135684"></amp-twitter>
   </body>
 </html>
 
